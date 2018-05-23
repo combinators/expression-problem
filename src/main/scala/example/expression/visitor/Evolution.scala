0cf1a2aebb5742499452f73d5da2c41646e5b0d0
George Heineman <heineman@cs.wpi.edu>
2018-05-22 23:37:35 -0400

    Even simpler! Now need to add code generation to the test cases and then this is done.

--- src/main/scala/example/expression/visitor/Evolution.scala
+++ src/main/scala/example/expression/visitor/Evolution.scala
@@ -1,8 +1,6 @@
 package example.expression.visitor
 
-
 import com.github.javaparser.ast.CompilationUnit
-import example.expression.ExpressionDomain
 import expression.history.History
 import expression.instances.UnitSuite
 import expression.tests.AllTests
@@ -15,7 +13,6 @@ import expression.{DomainModel, Exp}
 import shared.compilation.{CodeGenerationController, HasCodeGenerator}
 
 import scala.collection.JavaConverters._
-
 
 abstract class Foundation @Inject()(web: WebJarsUtil, app: ApplicationLifecycle)
   extends CodeGenerationController[CompilationUnit](web, app)
@@ -73,60 +70,37 @@ class E0_Variation @Inject()(web: WebJarsUtil, app: ApplicationLifecycle)
 }
 
 class E1_Variation @Inject()(web: WebJarsUtil, app: ApplicationLifecycle)
-  extends E0_Variation(web, app) with e0.Model with e1.Model {
+  extends E0_Variation(web, app) with e1.Model {
 
   override lazy val controllerAddress = "e1"
 
-  // start off the domain Model. Subsequent subclasses merge their domain models with ours, to satisfy the
-  // demands of the visitor solution to the expression problem.
   override def history:History = evolution.E1.extend(super.history)
-
-  //def domain:DomainModel = history.flatten
-  // all tests are derived from the model.
   override def testCases:UnitSuite = tests.e1.TestCases.add(super.testCases)
 }
 
 class E2_Variation @Inject()(web: WebJarsUtil, app: ApplicationLifecycle)
-  extends E1_Variation(web, app) with e0.Model with e1.Model with e2.Model {
+  extends E1_Variation(web, app) with e2.Model {
 
   override lazy val controllerAddress = "e2"
 
-  // start off the domain Model. Subsequent subclasses merge their domain models with ours, to satisfy the
-  // demands of the visitor solution to the expression problem.
   override def history:History = evolution.E2.extend(super.history)
-
-  //def domain:DomainModel = history.flatten
-  // all tests are derived from the model.
   override def testCases:UnitSuite = tests.e2.TestCases.add(super.testCases)
 }
 
 class E3_Variation @Inject()(web: WebJarsUtil, app: ApplicationLifecycle)
-  extends E2_Variation(web, app) with e0.Model with e1.Model with e2.Model with e3.Model {
+  extends E2_Variation(web, app) with e3.Model {
 
   override lazy val controllerAddress = "e3"
 
-  // start off the domain Model. Subsequent subclasses merge their domain models with ours, to satisfy the
-  // demands of the visitor solution to the expression problem.
   override def history:History = evolution.E3.extend(super.history)
-
-  //def domain:DomainModel = history.flatten
-  // all tests are derived from the model.
   override def testCases:UnitSuite = tests.e3.TestCases.add(super.testCases)
 }
 
 class E4_Variation @Inject()(web: WebJarsUtil, app: ApplicationLifecycle)
-  extends E3_Variation(web, app) with e0.Model with e1.Model with e2.Model with e3.Model with e4.Model {
+  extends E3_Variation(web, app) with e4.Model {
 
   override lazy val controllerAddress = "e4"
 
-  // start off the domain Model. Subsequent subclasses merge their domain models with ours, to satisfy the
-  // demands of the visitor solution to the expression problem.
   override def history:History = evolution.E4.extend(super.history)
-
-  //def domain:DomainModel = history.flatten
-  // all tests are derived from the model.
   override def testCases:UnitSuite = tests.e4.TestCases.add(super.testCases)
 }
-
-// TODO: I bet these could be automagically constructed right from the "e1" .. "e4" in the URL for
-// TODO: the variation (though the rep definition might be a challenge).

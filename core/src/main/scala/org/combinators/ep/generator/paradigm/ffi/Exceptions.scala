5969c8296a5cfff013da6289a9086e10b58f09fc
George Heineman <heineman@cs.wpi.edu>
2021-03-01 13:30:50 -0500

    Missing file

---
+++ core/src/main/scala/org/combinators/ep/generator/paradigm/ffi/Exceptions.scala
@@ -0,0 +1,28 @@
+package org.combinators.ep.generator.paradigm.ffi
+
+/*DI:LI:AI*/
+
+import org.combinators.ep.generator.Command.Generator
+import org.combinators.ep.generator.{Command, Understands}
+import org.combinators.ep.generator.paradigm.{AnyParadigm, Apply}
+
+case class Exception[Expression,Stmt](exp:Expression) extends Command {
+  type Result = Stmt
+}
+
+trait Exceptions[Context] extends FFI {
+  import base.syntax._
+
+  trait ExceptionCapabilities {
+    implicit val canRaise: Understands[Context, Exception[Expression, Statement]]
+
+    def raise(exp: Expression): Generator[Context, Statement] =
+      AnyParadigm.capabilitiy(Exception[Expression, Statement](exp))
+  }
+  
+  val exceptionCapabilities: ExceptionCapabilities
+}
+
+object Exceptions {
+  type WithBase[Ctxt, B <: AnyParadigm] = Exceptions[Ctxt] { val base: B }
+}

4ef17411a81c40cdfb1f41f5359137f37c76c0a6
George Heineman <heineman@cs.wpi.edu>
2022-02-17 23:34:34 -0500

    prep script to generate everything

--- scripts/runAll.bat
+++ scripts/runAll.bat
@@ -7,27 +7,30 @@ cd ..
 set SAVED_JAVA_HOME=%JAVA_HOME%
 
 for %%a in (oo visitor visitorSideEffect extensibleVisitor interpreter coco trivially dispatch) do (
+  @echo off
   echo %%a
-  echo %%a > jacoco.%%a
+  echo %%a > scripts\jacoco.%%a
 
-  for /F "tokens=2" %%i in ('date /t') do set mydate=%%i
-  set mytime=%time%
-  echo Current time is %mydate%:%mytime% >> jacoco.%%a
+  echo Current time is %date% @ %time% >> scripts\jacoco.%%a
 
   @REM for each approach x model, execute to generate into target\ep2
   for %%e in (M0 J1 J2 J3 J4 J5 J6 J7 J8 J5J8 J9 J10) do (
+     @echo off
+     echo ====================================== >> scripts\jacoco.%%a
+     echo %%e                                    >> scripts\jacoco.%%a
+     echo ====================================== >> scripts\jacoco.%%a
      set JAVA_HOME=%SAVED_JAVA_HOME%
 
      sbt "language-java/runMain org.combinators.ep.language.java.DirectToDiskMainJ %%a %%e"
 
      @REM generated into target\ep2
      cd target\ep2
-     zip -r ..\%%a-%%e-src.zip src
-     echo %%e >> jacoco.%%a
+     zip -r ..\..\scripts\%%a-%%e-src.zip src
+     
      set JAVA_HOME=C:\Program Files\AdoptOpenJDK\jdk-8.0.212.03-hotspot
 
-     sbt jacoco >> jacoco.%%a
-     echo >> jacoco.%%a
+     sbt jacoco            >> ..\..\scripts\jacoco.%%a
+     echo                  >> ..\..\scripts\jacoco.%%a
 
      @REM back up to main
      cd ..\..

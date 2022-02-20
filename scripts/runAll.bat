0812b214a9e97badc7d0a2e66a01cd8d518d1ef9
George Heineman <heineman@cs.wpi.edu>
2022-02-20 14:13:46 -0500

    trying to unbias first run

--- scripts/runAll.bat
+++ scripts/runAll.bat
@@ -9,9 +9,12 @@ cd ..
 
 mkdir target\analysis
 
+@REM burn one run, to ensure no bias from "first one run"
+sbt "language-java/runMain org.combinators.ep.language.java.DirectToDiskMainJ coco j10"
+
 set SAVED_JAVA_HOME=%JAVA_HOME%
 
-for %%a in (oo visitor visitorSideEffect extensibleVisitor interpreter coco trivially dispatch) do (
+for %%a in (oo visitor visitorSideEffect extensibleVisitor interpreter dispatch trivially coco) do (
   @echo off
   echo %%a
   echo %%a > target\analysis\jacoco.%%a
@@ -20,7 +23,7 @@ for %%a in (oo visitor visitorSideEffect extensibleVisitor interpreter coco triv
   for %%e in (M0 J1 J2 J3 J4 J5 J6 J7 J8 J5J8 J9 J10) do (
      @echo off
      echo ====================================== >> target\analysis\jacoco.%%a
-     echo %%e                                    >> target\analysis\jacoco.%%a
+     echo %%e-Generate                           >> target\analysis\jacoco.%%a
      java -cp scripts Time                       >> target\analysis\jacoco.%%a
      echo ====================================== >> target\analysis\jacoco.%%a
      set JAVA_HOME=%SAVED_JAVA_HOME%
@@ -32,9 +35,18 @@ for %%a in (oo visitor visitorSideEffect extensibleVisitor interpreter coco triv
      zip -r ..\analysis\%%a-%%e-src.zip src
      
      set JAVA_HOME=C:\Program Files\AdoptOpenJDK\jdk-8.0.212.03-hotspot
+     echo ====================================== >> ..\analysis\jacoco.%%a
+     echo %%e-Test-Begin                         >> ..\analysis\jacoco.%%a
+     java -cp ..\..\scripts Time                 >> ..\analysis\jacoco.%%a
+     echo ====================================== >> ..\analysis\jacoco.%%a
 
      sbt jacoco            >> ..\analysis\jacoco.%%a
      echo                  >> ..\analysis\jacoco.%%a
+
+     echo ====================================== >> ..\analysis\jacoco.%%a
+     echo %%e-Test-End                           >> ..\analysis\jacoco.%%a
+     java -cp ..\..\scripts Time                 >> ..\analysis\jacoco.%%a
+     echo ====================================== >> ..\analysis\jacoco.%%a
 
      @REM back up to main
      cd ..\..

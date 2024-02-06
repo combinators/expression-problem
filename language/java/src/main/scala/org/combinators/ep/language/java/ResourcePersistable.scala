823a398bd47a90190736473057488d1988cfe12e
heineman <heineman@wpi.edu>
2024-02-06 09:52:38 -0500

    Notes on potentially downgrading some code to JDK8

--- language/java/src/main/scala/org/combinators/ep/language/java/ResourcePersistable.scala
+++ language/java/src/main/scala/org/combinators/ep/language/java/ResourcePersistable.scala
@@ -10,8 +10,16 @@ trait ResourcePersistableInstances {
     override type T = BundledResource
     override def path(elem: BundledResource): Path = elem.persistTo
     override def rawText(elem: BundledResource): Array[Byte] = {
-      val contents = elem.classToLoadResource.getResourceAsStream(elem.name)
+      val contents:java.io.InputStream = elem.classToLoadResource.getResourceAsStream(elem.name)
+      // TODO: This method is JDK9; consider replacing with JDK-8 equivalent?
       contents.readAllBytes()     // requires JDK 9 or higher
+
+      // could be replaced with:
+      //      contents.reset()
+      //      val bytes:Array[Byte] = new Array[Byte](contents.available())
+      //      new java.io.DataInputStream(contents).readFully(bytes)
+      //      bytes
+
     }
   }
 }

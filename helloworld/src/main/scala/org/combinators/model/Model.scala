92e2062c5466492a3a8d42e7b3d148640b120d8f
Matthew Samuels <mlsamuels@wpi.edu>
2026-01-20 17:38:43 -0500

    Update Model.scala
    Test Commit

--- helloworld/src/main/scala/org/combinators/model/Model.scala
+++ helloworld/src/main/scala/org/combinators/model/Model.scala
@@ -2,5 +2,4 @@ package org.combinators.model
 
 class Model(val problem:String, val bounds: List[Expression], val cases: List[(Option[Expression], Expression)])
 
-
 // test example in bottomUp/twoSequences/longestcommonsubsequence/LongestCommonSubsequenceObjectOrientedProvider.scala

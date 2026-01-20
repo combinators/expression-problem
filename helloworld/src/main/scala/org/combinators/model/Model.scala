a88b1c2373c742660694b2792d65f1bd89640980
Matthew Samuels <mlsamuels@wpi.edu>
2026-01-20 17:37:27 -0500

    Removed Todo

--- helloworld/src/main/scala/org/combinators/model/Model.scala
+++ helloworld/src/main/scala/org/combinators/model/Model.scala
@@ -2,6 +2,5 @@ package org.combinators.model
 
 class Model(val problem:String, val bounds: List[Expression], val cases: List[(Option[Expression], Expression)])
 
-// todo: figure out how to represent transitions (subproblems)
 
 // test example in bottomUp/twoSequences/longestcommonsubsequence/LongestCommonSubsequenceObjectOrientedProvider.scala

7645ddaac8fbba73e0af97e7977a4e8ff1f22381
cacsibal <cacsibal@gmail.com>
2026-01-20 17:16:20 -0500

    re-implemented lcs

--- helloworld/src/main/scala/org/combinators/model/Setup.scala
+++ helloworld/src/main/scala/org/combinators/model/Setup.scala
@@ -3,21 +3,20 @@ package org.combinators.model
 class Setup {
   def instantiate(): Model = {
 
-    val s1= new ArgExpression(0)
+    val s1 = new ArgExpression(0)
     val s2 = new ArgExpression(1)
 
-    val boundZero: Expression =new StringLengthExpression(new ArgExpression(0))
-    val boundOne: Expression =new StringLengthExpression(new ArgExpression(1))
-    val bounds = List(boundZero,boundOne)
+    val boundZero: Expression = new StringLengthExpression(new ArgExpression(0))
+    val boundOne: Expression = new StringLengthExpression(new ArgExpression(1))
+    val bounds = List(boundZero, boundOne)
 
     val r: IteratorExpression = new IteratorExpression(0)
     val c: IteratorExpression = new IteratorExpression(1)
-    val iterators = List(r, c)
 
     val zero: LiteralInt = new LiteralInt(0)
     val one: LiteralInt = new LiteralInt(1)
 
-    val LCS: Model = new Model("Prototype",
+    val LCS: Model = new Model("Prototype LCS",
       bounds,
       cases = List(
         (
@@ -30,11 +29,17 @@ class Setup {
         ),
         (
           Some(new EqualExpression(new CharAtExpression(s1, r), new CharAtExpression(s2, c))),
-          Some()
+          new AdditionExpression(
+            new SubproblemExpression(new SubtractionExpression(r, one), new SubtractionExpression(c, one)),
+            one
+          )
         ),
         (
           None,
-          Some()
+          new MaxExpression(
+            new SubproblemExpression(r, new SubtractionExpression(c, one)),
+            new SubproblemExpression(new SubtractionExpression(r, one), c)
+          )
         )
       )
     )
@@ -44,11 +49,11 @@ class Setup {
 
     val two: LiteralInt = new LiteralInt(2)
 
-    val bound=List(new ArgExpression(0))
+    val bound = List(new ArgExpression(0))
 
     val i: IteratorExpression = new IteratorExpression(0)
-    val im1 = new SubtractionExpression(i,one)
-    val im2 = new SubtractionExpression(i,two)
+    val im1 = new SubtractionExpression(i, one)
+    val im2 = new SubtractionExpression(i, two)
 
 
     val Fib: Model = new Model("Fibonacci",

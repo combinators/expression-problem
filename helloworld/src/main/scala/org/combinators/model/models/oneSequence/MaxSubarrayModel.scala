0be5dc6d00cffec416351b302299ed25bff0c5e3
Matthew Samuels <mlsamuels@wpi.edu>
2026-01-29 13:28:02 -0500

    Changed MaxSubarrayModel to use extra dimension

--- helloworld/src/main/scala/org/combinators/model/models/oneSequence/MaxSubarrayModel.scala
+++ helloworld/src/main/scala/org/combinators/model/models/oneSequence/MaxSubarrayModel.scala
@@ -8,24 +8,29 @@ class MaxSubarrayModel {
     val zero: LiteralInt = new LiteralInt(0)
     val one: LiteralInt = new LiteralInt(1)
 
-//    val bounds = List(new ArrayLengthExpression(new ArgExpression(0,"nums",new IntegerType)))
-    val bounds = List(new ArgExpression(0, "n", new IntegerType()))
+    val bounds = List(new ArrayLengthExpression(new ArgExpression(0,"nums",new IntegerType)), 2)
+    val fakeBound = List(new ArgExpression(0,"nums",new IntegerType))
 
     val i: IteratorExpression = new IteratorExpression(0, "i")
+    val m: IteratorExpression = new IteratorExpression(0, "m")
 
     val cur= new ArrayElementExpression(new ArgExpression(0,"nums",new IntegerType), i)
 
 
     val MaxSubarray: Model = new Model("MaxSubarray",
-      bounds,
+      fakeBound,
       cases = List(
         (
-          Some(new EqualExpression(i, zero)),
+          Some( new EqualExpression(i, zero)),
           cur
         ),
         (
+          Some(new EqualExpression(m,zero)),
+          new MaxExpression(new SubproblemExpression(Seq(i-one,zero))+cur, cur)
+        ),
+        (
           None,
-          new MaxExpression(new SubproblemExpression(Seq(i-one))+cur, cur)
+          new MaxExpression(new SubproblemExpression(Seq(i,zero)),new SubproblemExpression(Seq(i-one,one)))
         )
       )
     )
